c361c701a07733d839d1654fa537c66e078b70fa
Keaton Laney <keatonus@gmail.com>
2020-06-24 11:22:30 -0600

    fix the test

--- index.a.test.js
+++ index.a.test.js
@@ -2,5 +2,5 @@ const request = require("supertest")
 const app = require("./index");
 test("Should have a hello world route", async () => {
     const response = await request(app).get("/").expect(200);
-    expect(response.body).toEqual("hello world")
+    expect(response.text).toEqual("hello world")
 })
